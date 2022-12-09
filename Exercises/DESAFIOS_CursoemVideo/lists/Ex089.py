21f2fb6561c44e392c8b479ab54fa1271c686c9d
Gilnei <menna0red@gmail.com>
2022-12-08 23:55:25 -0300

    Create Ex089.py

---
+++ Exercises/DESAFIOS_CursoemVideo/lists/Ex089.py
@@ -0,0 +1,9 @@
+lista = list()
+
+nome = str(input('Nome: '))
+nota1 = float(input('Nota 1: '))
+nota2 = float(input('Nota 2: '))
+media = (nota1 + nota2) / 2
+lista.append([nome, [nota1, nota2], media])
+
+print(lista)
